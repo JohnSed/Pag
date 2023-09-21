2118ee739eec13d39028727d0d09428e695df788
JohnSed <JOHND3DS@GMAIL.COM>
2023-09-20 23:49:52 -0500

    actua

--- jvs.js
+++
@@ -1,6 +0,0 @@
-<script>
-function pausarVideo() {
-    var video = document.getElementById('videoPlayer');
-    video.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
-}
-</script>
