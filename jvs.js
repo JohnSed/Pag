c89cbfd998b9d9e0995203e1fca22953f1fe86ef
JohnSed <JOHND3DS@GMAIL.COM>
2023-09-20 23:44:36 -0500

    mi

--- jvs.js
+++ jvs.js
@@ -1,11 +1,6 @@
-let nombre= "Jhon";
-var apellido ="Diaz";
-const nombre2 ="Agredo";
-console.log (nombre);
-console.log (apellido);
-console.log (nombre2);
-// Seleccionar Elementos
-let contenidotitulo ="Logo";
-let titulo = document.querySelector("logo ,fuente")
-const imprimir = (texto)=>{console.log(texto)}
-imprimir("HolaMundo");
+<script>
+function pausarVideo() {
+    var video = document.getElementById('videoPlayer');
+    video.contentWindow.postMessage('{"event":"command","func":"' + 'pauseVideo' + '","args":""}', '*');
+}
+</script>
